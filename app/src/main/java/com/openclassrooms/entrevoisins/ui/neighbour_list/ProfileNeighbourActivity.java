eaecfd762f2317e5cb3113bf8a1816856f5e0860
Arthur Laplace <art.laplace@gmail.com>
2021-08-27 16:02:08 +0200

    Navigation and SetData Working! Updated
    TODO:
    - polish activity_profile_neighbour.xml (colors, icons).
    - create the favorite widget and implement it.
    
    OPTIONAL-TODO:
    - sliding animation between activities.
    - neighbour database update upon creation or deletion of a neighbour, at start() and finish().

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ProfileNeighbourActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ProfileNeighbourActivity.java
@@ -1,11 +1,11 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
-import android.annotation.SuppressLint;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.content.res.Resources;
 
 import com.bumptech.glide.Glide;
 import com.openclassrooms.entrevoisins.R;
@@ -28,14 +28,15 @@ public class ProfileNeighbourActivity extends AppCompatActivity {
             String neighbourAdresse = getIntent().getStringExtra("neighbour_adresse");
             String neighbourPhone = getIntent().getStringExtra("neighbour_phone");
             String neighbourAboutMe = getIntent().getStringExtra("neighbour_aboutMe");
+            String neighbourMedia = getString(R.string.social, neighbourName);
 
             setData(neighbourUrl, neighbourName, neighbourAdresse,
-                    neighbourPhone, neighbourAboutMe);
+                    neighbourPhone, neighbourAboutMe, neighbourMedia);
         }
     }
 
     private void setData(String neighbourUrl, String neighbourName, String neighbourAdresse,
-                         String neighbourPhone, String neighbourAboutMe) {
+                         String neighbourPhone, String neighbourAboutMe, String neighbourMedia) {
 
         ImageView headerAvatar = findViewById(R.id.profile_header_avatar);
         Glide.with(this)
@@ -52,6 +53,7 @@ public class ProfileNeighbourActivity extends AppCompatActivity {
         bodyPhone.setText(neighbourPhone);
         TextView bioAboutMe = findViewById(R.id.profile_bio_corpus);
         bioAboutMe.setText(neighbourAboutMe);
-
+        TextView bodyMedia = findViewById(R.id.profile_body_sociale_content);
+        bodyMedia.setText(neighbourMedia);
     }
 }
